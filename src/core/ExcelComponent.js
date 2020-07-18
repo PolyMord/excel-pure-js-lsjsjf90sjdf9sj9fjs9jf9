178142f45ec3174f9ce831f7b4e1a15aeb290bb1
PolyMord <stanislavmalickiy@gmail.com>
2020-07-19 01:40:33 +0300

    finish redux and table

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -5,6 +5,8 @@ export class ExcelComponent extends DOMListener {
     super($root, options.listeners)
     this.name = options.name || ''
     this.emitter = options.emitter
+    this.subscribe = options.subscribe || []
+    this.store = options.store
     this.unsubscribers = []
 
     this.prepare()
@@ -25,8 +27,18 @@ export class ExcelComponent extends DOMListener {
     this.unsubscribers.push(unsub)
   }
 
+  $dispatch(action) {
+    this.store.dispatch(action)
+  }
+
+  storeChanged() {}
+
   init() {
     this.initDOMListeners()
+  }
+
+  isWatching(key) {
+    return this.subscribe.includes(key)
   }
 
   destroy() {
