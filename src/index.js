faeeb6d0258e0d7bbbdb2a26fe1718dc28fef4de
PolyMord <stanislavmalickiy@gmail.com>
2020-07-21 05:50:21 +0300

    implement DIP

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { DashboardPage } from '@/pages/DashboardPage'
 import { Router } from '@core/routes/Router'
-import './scss/index.scss'
 import { ExcelPage } from '@/pages/ExcelPage'
+import './scss/index.scss'
 
 new Router('#app', {
   dashboard: DashboardPage,
